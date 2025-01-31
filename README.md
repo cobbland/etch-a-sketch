3e19c754c813fbad95d2a1c38f598f6092e83e3c
cobbland <jacobdensford@proton.me>
2025-01-31 07:59:54 -0500

    Finish project
    I neglected to make more detailed periodic commits.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Build a browser version of something between a sketchpad and an Etch-A-Sketch.
 
-- [ ] Create a webpage with a 16x16 grid of square divs.
-- [ ] Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
-- [ ] Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
+- [x] Create a webpage with a 16x16 grid of square divs.
+- [x] Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+- [x] Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //Declare constants and variables
-const buttons = document.querySelector("#buttons");
 const drawBox = document.querySelector("#drawBox");
+const clearButton = document.querySelector("#clearBox");
+const resizeButton = document.querySelector("#setSquares");
 
 // Create divs for drawing
 function createDivs(divsNum) {
+    removeDivs(drawBox);
     for (let step = 0; step < (divsNum * divsNum); step++) {
         const div = document.createElement("div");
         div.setAttribute("class", "undrawnBox");
@@ -15,6 +17,37 @@ function createDivs(divsNum) {
     return 
 };
 
+// Clear divs
+function clearDivs(drawBox) {
+    for (const child of drawBox.childNodes) {
+        child.removeAttribute("class", "drawnBox");
+    };
+};
+
+// Remove divs
+function removeDivs(drawBox) {
+    while (drawBox.firstChild) {
+        drawBox.removeChild(drawBox.firstChild);
+    };
+};
+
 // Change div style on hover
+drawBox.addEventListener("mouseover", (event) => {
+    event.target.setAttribute("class", "drawnBox");
+});
 
 // Make buttons do their things
+clearButton.addEventListener("click", () => {
+    clearDivs(drawBox);
+});
+
+resizeButton.addEventListener("click", () => {
+    let newPixels = parseInt(prompt("How many 'pixels' per side?"));
+    while (!newPixels || newPixels > 100) {
+        newPixels = parseInt(prompt("Must be a number 100 or lower."));
+    };
+    createDivs(newPixels);
+});
+
+// Start with 16 x 16
+createDivs(16);
